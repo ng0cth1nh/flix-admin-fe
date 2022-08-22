cecfd18195c0a8c353b0219d336d58839d0c130c
Vu Ngoc Thinh (K14 HL) <thinhvnhe141345@fpt.edu.vn>
2022-08-22 03:28:53 +0000

    ListCategoriesPage.jsx edited online with Bitbucket

--- src/pages/category/ListCategoriesPage.jsx
+++ src/pages/category/ListCategoriesPage.jsx
@@ -1,4 +1,4 @@
-import "./listCategories.scss";
+import "./ListCategories.scss";
 import { useState, useEffect } from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
